28084a3e8003a2d6d040f64a30902d48d3a40e36
YUJIN LEE <74438259+Yujin-17@users.noreply.github.com>
2023-02-15 19:17:43 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/70128. 내적/내적.java
@@ -0,0 +1,12 @@
+    class Solution {
+        public int solution(int[] a, int[] b) {
+            int answer = 0;
+            for(int i = 0; i < a.length; i++){
+                int c = 0;
+                c = a[i] * b[i];
+                answer += c;
+            }
+
+            return answer;
+        }
+    }
