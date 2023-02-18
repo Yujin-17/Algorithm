a900b86734f1561fde15713b1db47f1bf631f030
YUJIN LEE <74438259+Yujin-17@users.noreply.github.com>
2023-02-18 20:15:07 +0900

    [level 1] Title: 3진법 뒤집기, Time: 8.91 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,16 @@
+class Solution {
+        public static int solution(int n) {
+
+            String answer = "";
+
+            while(n >= 3)
+            {
+                answer += n % 3;
+                n /= 3;
+            }
+            answer += n;
+
+
+            return Integer.parseInt(answer, 3);
+        }
+    }
