8a7004929c2089d63370dccc8a770f16b03989e7
YUJIN LEE <74438259+Yujin-17@users.noreply.github.com>
2023-03-08 02:12:30 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.48 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+class Solution {
+    public String solution(String s) {
+        
+        String[] arr = s.split(" ");
+       
+        int min = Integer.parseInt(arr[0]);
+        int max = Integer.parseInt(arr[0]);
+        for(int i = 0; i < arr.length; i++){
+            int num = Integer.parseInt(arr[i]);
+            if(num < min){
+                min = num;
+            }
+            if(num > max){
+                max = num;
+            }
+        }
+        
+        return min + " " + max;
+    }
+}
