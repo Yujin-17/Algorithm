92261c1ae5d9b458ed3f282616926c749058946a
YUJIN LEE <74438259+Yujin-17@users.noreply.github.com>
2023-03-08 01:44:52 +0900

    [level 1] Title: 예산, Time: 0.34 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for (int i = 0; i <= d.length -1; i++) { 
+            if(budget >= d[i]){
+                budget -= d[i];
+                answer++;
+            }else break;
+           
+            
+        }
+        return answer;
+    }
+
+}
