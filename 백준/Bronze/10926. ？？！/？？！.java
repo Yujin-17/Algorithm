3aff33318243472e977c9dd15b0428e2c213440e
YUJIN LEE <74438259+Yujin-17@users.noreply.github.com>
2023-03-09 02:26:45 +0900

    [Bronze V] Title: ??!, Time: 112 ms, Memory: 12972 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //input
+        Scanner scanner = new Scanner(System.in);
+//        int first = scanner.nextInt();
+//        int second = scanner.nextInt();
+        String a = scanner.nextLine();
+        //logic
+
+        System.out.println(a + "??!");
+
+        //output
+//        System.out.println(answer);
+    }
+}
