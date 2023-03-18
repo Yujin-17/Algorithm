30c3a1e2c98e4bc82fb1f01d69c945c4be7d7bdc
YUJIN LEE <74438259+Yujin-17@users.noreply.github.com>
2023-03-19 01:03:09 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;    
+class Solution {
+        public ArrayList solution(int n) {
+            ArrayList answer = new ArrayList();
+            
+            for(int i = 0; i <= n; i++){
+                if(i % 2 != 0){
+                    answer.add(i);
+                }
+            }
+            return answer;
+        }
+    }
